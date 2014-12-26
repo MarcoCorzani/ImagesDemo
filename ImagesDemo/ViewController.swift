f384e5aea854d897c33e7f1f815a7799abf19f39
Marco <Marco@Marcos-iMac.fritz.box>
2014-12-26 22:54:02 +0100

    1

--- ImagesDemo/ViewController.swift
+++ ImagesDemo/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,39 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func imageButtonPressed(sender: AnyObject) {
+    
+    
+        var imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        var sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum   //Camera mal versuchen
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+        
+        imagePicker.sourceType = sourceType
+            
+        //show a screen
+            
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+        }
+        
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        var image = info[UIImagePickerControllerOriginalImage] as UIImage
+        
+        imageView.image = image
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        println("Cancel")
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
